b47149a422979f0dc08b26609ce2f4b52baa385d
dannysaleeb <dannysaleeb@gmail.com>
2025-11-21 14:51:33 +0000

    added temporary usage of NoteName

--- src/classes/Fundamental.js
+++ src/classes/Fundamental.js
@@ -5,22 +5,26 @@ class Partial {
         this.frequency = partialNumber * fundamental.frequency;
 
         // placeholder
-        this.notes = ['c', 'd', 'e', 'f', 'g', 'a', 'b'];
+        this.notes = [];
+        for (let i=0; i < 7; i++) {
+            this.notes.push(new NoteName(i, 0))
+        };
 
+        // placeholder
         this.noteName = this.getNoteName();
     }
 
     getNoteName() {
-        let length = this.notes.length;
-        return this.notes[this.partialNumber % length];
+        return this.notes[this.partialNumber % 7];
     }
 }
 
 class Fundamental {
-  constructor(frequency, noteName) {
+  constructor(frequency, noteName, octave) {
     this.frequency = frequency;
     this.noteName = noteName;
     this.keySig = this.getKeySig(noteName);
+    this.octave = octave;
   }
 
   getPartial(n) {
@@ -47,7 +51,7 @@ class NoteName {
 
 // take it that these selections are made:
 const button = {partialNumber: 3}; // represents partialNum 3 button pressed
-const fundamentalContext = new Fundamental(55, new NoteName(6, 0)) // represents low A-nat fundamental selected 
+const fundamentalContext = new Fundamental(55, new NoteName(6, 0), 1) // represents low A-nat fundamental selected 
 //
 
 // need to consider how NoteName is assigned to fundamental on keyboard press
@@ -56,4 +60,7 @@ const partial = new Partial(button.partialNumber, fundamentalContext);
 
 console.log(`Fundamental frequency is: ${partial.fundamental.frequency}Hz`);
 console.log(`Selected partial (3) frequency is: ${partial.frequency}Hz`);
+console.log(`Selected partial note name is:`);
+console.log(partial.noteName);
+
 console.log(`Frequency of 7th partial is: ${fundamentalContext.getPartial(7).frequency}Hz`);
