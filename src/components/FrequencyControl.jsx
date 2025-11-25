cba96b3a064369318f70f1294f3370cee4667241
fifoh <125158808+fifoh@users.noreply.github.com>
2025-11-25 22:54:04 +0000

    Update FrequencyControl.jsx

--- src/components/FrequencyControl.jsx
+++ src/components/FrequencyControl.jsx
@@ -1,1 +1,4 @@
-// problems with feedback loops updating the fundamental - react doesn't think the fundamental has changed if you just change the freq?
+// to replace CentDeviationControl, deal with all adjustments
+
+// problems with feedback loops updating the fundamental though - react doesn't think the fundamental has changed if you just change the freq?
+// but telling react it has changed causes a feedback loop in the app --- 
